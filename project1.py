1f35ea936d5153b9e5f99393dda4d01d4127d3ab
LucyIbe <ibelucyifeosoluchukwu@gmail.com>
2022-11-06 21:37:16 +0000

    prints users input

---
+++ project1.py
@@ -0,0 +1,7 @@
+def hello():
+    name=str(input("enter your name : "))
+    age=str(input("enter your age: "))
+    department=str(input("enter your department: "))
+    print("Hi, my name is " + name + ", and I am " + age + " years old, in " + department + " Department.")
+    return
+hello()
